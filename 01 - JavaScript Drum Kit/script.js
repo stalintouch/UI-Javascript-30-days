619524061056380ecdad7937a400ea6715ac6407
stalintouch <stalintouch1983@gmail.com>
2018-03-30 19:18:59 -0400

    finished up drumkit

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,3 +1,25 @@
+const obj = {
+  a: "a",
+  b: "b"
+}
+
+const {a,b} = obj;
+
 window.addEventListener('keydown', e => {
-  keys.querySelectAll()
-});
+  
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+  if(!audio) return;
+
+  audio.currentTime =0;
+
+  key.classList.add('playing');
+
+  audio.play();
+
+  key.addEventListener('transitionend', (e) => {
+     key.classList.remove('playing');
+  });
+
+}); 
