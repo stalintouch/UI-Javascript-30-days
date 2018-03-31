761203f0119d5bc225850583a6bc06cc476122b4
stalintouch <stalintouch1983@gmail.com>
2018-03-30 22:53:42 -0400

    removed eslint

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,25 +1,18 @@
-const obj = {
-  a: "a",
-  b: "b"
-}
+const { a, b } = obj;
 
-const {a,b }  = obj;
-
-window.addEventListener('keydown', e => {
-  
+window.addEventListener("keydown", e => {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
-  if(!audio) return;
+  if (!audio) return;
 
-  audio.currentTime =0;
+  audio.currentTime = 0;
 
-  key.classList.add('playing');
+  key.classList.add("playing");
 
   audio.play();
 
-  key.addEventListener('transitionend', (e) => {
-     key.classList.remove('playing');
+  key.addEventListener("transitionend", e => {
+    key.classList.remove("playing");
   });
-
-}); 
+});
